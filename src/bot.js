f8d9a2d7c153cf9366a8e6368a8e8ef6a6e882e9
foday <foday_s@yahoo.com>
2021-05-15 20:02:50 -0500

    deploying discord app

--- src/bot.js
+++ src/bot.js
@@ -4,5 +4,15 @@ const { Client } = require('discord.js');
 // create a client object from a Client class
 const client = new Client();
 
+client.on('ready', () => {
+    console.log(client.user.tag+' has logged in.');
+});
+
+client.on('message', (message) => {
+ console.log(message.author.tag + message.content);
+ if (message.content === 'hello') {
+     message.reply('Ahoy');
+}
+});
 // referenceing the login method from the Client class
 client.login(process.env.DISCORDJS_BOT_TOKEN);
