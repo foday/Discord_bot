a9328d4db16d7f12c3d0c067e9f1620a7ebc3b2c
foday <foday_s@yahoo.com>
2021-05-15 19:16:27 -0500

    Creating the discord app, adding bot to our discord server, logging the bot in
    I created a server in the discord website and called it discord bot tutorial, then I went to the url add the client id and scope with bot at the end to tell the site that i want to create a bot, i logged in to the bot. I setup nodemon in the package.json so all i have to do from now on is type npm start.

--- src/bot.js
+++ src/bot.js
@@ -1,1 +1,8 @@
 require('dotenv').config();
+
+const { Client } = require('discord.js');
+// create a client object from a Client class
+const client = new Client();
+
+// referenceing the login method from the Client class
+client.login(process.env.DISCORDJS_BOT_TOKEN);
